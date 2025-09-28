d99bbbe0562b61ccdbcdd4da6fc5a5a0d6e3d490
AAHRF <mdh88@nycstudents.net>
2025-09-28 01:03:05 -0400

    added new code

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double bill = 4;
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println(bill);
-        System.out.println(bill * 4);
+        System.out.print("Enter your bill: ");
+        double bill = scan.nextDouble();
+        scan.nextLine();
+
+        System.out.print("Enter tip percent in whole numbers and without % sign: ");
+        int tip = scan.nextInt();
+        scan.nextLine();
+
+        System.out.print("Number of people: ");
+        int people = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println();
+        System.out.println("-----------------------------------------------");
+
+        double tipamount = bill * ((double)tip / 100); //gives the unestimated tip amount
+        double t = tipamount % 0.01;
+        double acctipamount = tipamount - t;
+
+        System.out.println(acctipamount);
+
+
+
+
+
+
+
+
+
 
 
 
