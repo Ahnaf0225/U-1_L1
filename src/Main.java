0944aa72bb31194000186cc1c57e435c1f09cf68
AAHRF <mdh88@nycstudents.net>
2025-09-27 23:17:36 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        double bill = 4;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println(bill);
+
+
+
+    }
+
+
+}
